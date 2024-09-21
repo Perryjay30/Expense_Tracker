5e0f5cf0dd9f3723d6038e3fd0b63200c3c604a1
Perryjay30 <pelumijsh@gmail.com>
2024-09-21 06:24:34 +0100

    Using dismissable widget for dismissing lists

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -26,7 +26,9 @@ class _ExpensesState extends State<Expenses> {
   ];
 
   void _openAddExpense() {
-    showModalBottomSheet(context: context,
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
   }
@@ -34,6 +36,12 @@ class _ExpensesState extends State<Expenses> {
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -54,7 +62,8 @@ class _ExpensesState extends State<Expenses> {
           children: [
             const Text('The chart'),
             Expanded(
-                child: ExpensesList(expenses: _registeredExpenses)
+                child: ExpensesList(expenses: _registeredExpenses,
+                  onRemoveExpense: _removeExpense,)
             ),
           ],
         )
