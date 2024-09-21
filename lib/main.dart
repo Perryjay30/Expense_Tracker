0a888fbc185ba646310d1b65e771462896528c46
Perryjay30 <pelumijsh@gmail.com>
2024-09-21 09:06:52 +0100

    Theming and using Color Scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
-    // theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.onPrimaryContainer
+        )
+      ),
       home: Expenses(),
     ),
   );
