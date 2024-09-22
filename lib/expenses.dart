53bd3194c2d6be68b6635a71819d36b54e15159e
Perryjay30 <pelumijsh@gmail.com>
2024-09-22 22:27:43 +0100

    Adding Chart widgets

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/new_expense.dart';
+import 'package:expense_tracker/widget/chart/chart.dart';
 import 'package:expense_tracker/widget/expense_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -85,10 +86,10 @@ class _ExpensesState extends State<Expenses> {
               ),
             ),
           ],
-        ),
+         ),
         body: Column(
           children: [
-            const Text('The chart'),
+            Chart(expenses: _registeredExpenses),
             Expanded(
                 child: mainContent
             ),
